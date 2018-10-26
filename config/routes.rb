940e693b3f2c7710855bee93851add10f25c87be
benghui <benghui@gmail.com>
2018-10-26 15:27:07 +0800

    models and routes completed, created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users do
+    resources :products do
+      resources :comments
+    end
+  end
+
+  resources :products do
+    resources :users
+    resources :comments
+    resources :categories
+  end
 
   root to: "home#index"
 end
