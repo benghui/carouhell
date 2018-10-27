3f609bab542f39ab2046709f65f93130f27640fd
xnithunx <nithun.nanda@gmail.com>
2018-10-27 16:16:12 +0800

    Updated the user model. Added foreign key to link buyer/seller id.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments 
-  has_many :products       
+  has_many :comments
+  has_many :products, foreign_key :seller_id
+  has_many :products, foreign_key :buyer_id
 end
