709a43a75335aff86013f1ff0846e84c77671640
traviegotswag <travisng@hotmail.sg>
2018-10-27 20:08:45 +0800

    Categories and Comments Controller up

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,16 @@
 class CategoriesController < ApplicationController
 
+
+    def index
+        @categories = Category.all
+      end
+    
+      def show
+        @category = Category.find(params[:id])
+        @product = @category.products
+      end
+
+
 #   def show
 #   end
 
