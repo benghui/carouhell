ecfa9b5c2c71f414e299df5cd3156f6e1c252568
benghui <benghui@gmail.com>
2018-10-28 22:43:13 +0800

    added username to users table and modified devise to utilize username for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
   has_many :comments
   has_many :products, foreign_key: "seller_id"
   has_many :products, foreign_key: "buyer_id"
+
+  def email_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
