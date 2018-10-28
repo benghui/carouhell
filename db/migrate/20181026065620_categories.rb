9d25df5450cdbcecc7fdd9d6707875e018e6d398
benghui <benghui@gmail.com>
2018-10-28 17:34:18 +0800

    modified migration, modified product model, modified categories & products controller, modified routes

--- db/migrate/20181026065620_categories.rb
+++ db/migrate/20181026065620_categories.rb
@@ -2,7 +2,6 @@ class Categories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :product_category
-      t.references :product
       t.timestamps
     end
   end
