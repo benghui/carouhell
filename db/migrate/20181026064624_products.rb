81def5d2fe7b39ad217c92f00e1d3ab9e3bcbcad
benghui <benghui@gmail.com>
2018-10-31 16:05:45 +0800

    altered migration to accept decimals

--- db/migrate/20181026064624_products.rb
+++ db/migrate/20181026064624_products.rb
@@ -2,7 +2,7 @@ class Products < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :product_name
-      t.integer :price
+      t.decimal :price, precision: 10, scale: 2
       t.references :seller, index: true, foreign_key: {to_table: :users}
       t.references :buyer, index: true, foreign_key: {to_table: :users}
       t.references :category
