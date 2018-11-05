ac765877571ae46c93c0aca885edf2b8ab4cf650
benghui <benghui@gmail.com>
2018-11-05 14:16:34 +0800

    deployment changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.middleware.use ChatActionCable 
+  config.web_socket_server_url = "wss://carouhell.herokuapp.com/"
 end
